285b87623956541ee44f08064fdc144a01975656
chalwemwansa <chalwemwansa23@gmail.com>
2024-01-13 11:47:24 +0200

    checking task2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -4,25 +4,32 @@
 """
 from fabric.api import local, env, put, run, cd
 from datetime import datetime
+from os.path import exists
 user = local('whoami', capture=True)
-env.hosts = [f'{user}@localhost', '3.84.255.85', '100.25.171.58']
+env.hosts = ['52.55.249.213', '54.157.32.137']
+#  env.hosts = ['3.84.255.85', '100.25.171.58']
 
 
 def do_pack():
     """ the function creates a tarball zip file (tgz) from the
         contents of the web_static directory
     """
-    env.user = local('whoami', capture=True)
+    env.user = user
     zip_file_name = datetime.now().strftime("web_static_%Y%m%d%H%M%S")
     target_folder = 'versions'
     source_folder = 'web_static'
+    print(type(zip_file_name))
 
     #  ensure the target folder exists and create it if it does not
-    local(f'if [ ! -d {target_folder} ]; then\
-            mkdir {target_folder};\
-            fi')
+    if not exists(target_folder):
+        local(f'mkdir -r {target_folder}')
     #  make the archive file
     local(f'tar -czvf {target_folder}/{zip_file_name}.tgz {source_folder}')
+
+    if exists(f'{target_folder}/{zip_file_name}.tgz'):
+        return f'{target_folder}/{zip_file_name}.tgz'
+    else:
+        return False
 
 
 def do_deploy(archive_path):
@@ -37,7 +44,7 @@ def do_deploy(archive_path):
                    echo 'True';\
                    fi", capture=True)
     if flag:
-        put(f'{archive_path}', '/tmp/')
+        put(archive_path, '/tmp/')
     else:
         return False
     flag = run(f"if [ ! -d {dest_folder} ]; then echo 'True'; fi")
@@ -49,12 +56,13 @@ def do_deploy(archive_path):
     with cd(f"{dest_folder}"):
         run(f'tar -xzf /tmp/{archive_name}')
     run(f'rm /tmp/{archive_name}')
-    run(f'mv {dest_folder}/web_static/* {dest_folder}')
     flag = run(f"if [ ! -f {link} ]; then echo 'True'; fi")
     if flag:
         pass
     else:
         run('rm -rf /data/web_static/current')
+    run(f'mv {dest_folder}/web_static/* {dest_folder}')
     run(f'rm -rf {dest_folder}/web_static')
     run(f'ln -sf {dest_folder} {link}')
+
     return True

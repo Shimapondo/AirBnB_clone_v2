71954e58b598c2725d124a47bebbda52c1630ac8
chalwemwansa <chalwemwansa23@gmail.com>
2024-01-13 13:16:45 +0200

    checking task2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -2,42 +2,14 @@
 """ the script that contains the necessary functions for deploying a
     website to a remote server
 """
-from fabric.api import local, env, put, run, cd
-from datetime import datetime
+from fabric.api import env, put, run
 from os.path import exists
 env.hosts = ['3.84.255.85', '100.25.171.58']
-
-
-def do_pack():
-    """ the function creates a tarball zip file (tgz) from the
-        contents of the web_static directory
-    """
-    env.user = local('whoami', capture=True)
-    zip_file_name = datetime.now().strftime("web_static_%Y%m%d%H%M%S")
-    target_folder = 'versions'
-    source_folder = 'web_static'
-
-    #  ensure the target folder exists and create it if it does not
-    local(f'if [ ! -d {target_folder} ]; then\
-            mkdir {target_folder};\
-            fi')
-    #  make the archive file
-    local(f'tar -czvf {target_folder}/{zip_file_name}.tgz {source_folder}')
-
-    flag = local(f"if [ -f '{target_folder}/{zip_file_name}.tgz' ]; then\
-                  echo 'True'; fi", capture=True)
-    if flag:
-        return f'{target_folder}/{zip_file_name}.tgz'
-    else:
-        return False
 
 
 def do_deploy(archive_path):
     """ function deploys an archive in the versions directory to a server
     """
-    # flag = local(f"if [ -f {archive_path} ]; then\
-    #               echo 'True';\
-    #               fi", capture=True)
     if exists(archive_path) is False:
         return False
     try:

d38ece86e7ced5e09279c204d5c699be1799bbf2
chalwemwansa <chalwemwansa23@gmail.com>
2024-01-27 10:34:06 +0200

    checking task3

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -28,8 +28,10 @@ def C(text):
     return f"C {escape(mod_string)}"
 
 
+@app.route('/python', defaults={'text', "is cool"},
+           strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def python(text="is cool"):
+def python(text):
     """displays python followed by a custom string where
        the default is 'is cool'
     """
